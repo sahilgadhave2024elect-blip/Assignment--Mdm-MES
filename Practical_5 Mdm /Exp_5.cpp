8a8f7a6d08cc1485ce27f8cbe23a2e6fecac18e0
sahilgadhave2024elect-blip <sahilgadhave2024.elect@mmcoe.edu.in>
2026-09-14 23:40:00 +0530

    Exp_5.cpp

--- Practical_5 Mdm /Exp_5.cpp
+++ Practical_5 Mdm /Exp_5.cpp
@@ -1,41 +1,44 @@
-
-//Timer1-Based Periodic LED Task with Pushbutton and I²C LCD
-
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-const int ledPin = 13;
-const int buttonPin = 7;
-
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+const byte LED_PIN = 13;
+const byte BUTTON_PIN = 7;
 
 volatile bool ledState = false;
 
+// Change 0x27 to 0x3F if required for your LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup()
 {
-  pinMode(ledPin, OUTPUT);
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
+  // Initialize LCD
   lcd.init();
   lcd.backlight();
 
   lcd.setCursor(0, 0);
-  lcd.print("Timer1 + Button");
+  lcd.print("Timer LED Blink");
 
   lcd.setCursor(0, 1);
   lcd.print("Button: RELEASED");
 
+  // Stop interrupts while configuring Timer1
   noInterrupts();
 
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1 = 0;
+  TCCR1A = 0;                   // Clear Timer1 control register A
+  TCCR1B = 0;                   // Clear Timer1 control register B
+  TCNT1  = 0;                   // Start counting from zero
 
-  // CTC mode, prescaler = 1024
+  // CTC mode: TOP value is stored in OCR1A
   TCCR1B |= (1 << WGM12);
-  TCCR1B |= (1 << CS12) | (1 << CS10);
 
-  // Approximately 1.5 second interrupt interval
+  // Prescaler = 1024
+  TCCR1B |= (1 << CS12);
+  TCCR1B |= (1 << CS10);
+
+  // Approximately 1.5-second interrupt interval
   OCR1A = 23437;
 
   // Enable Timer1 Compare Match A interrupt
@@ -44,37 +47,44 @@ void setup()
   interrupts();
 }
 
-ISR(TIMER1_COMPA_vect)
-{
-  ledState = !ledState;
-  digitalWrite(ledPin, ledState);
-}
-
 void loop()
 {
   static int previousButtonState = HIGH;
-  int buttonState = digitalRead(buttonPin);
 
-  if (buttonState != previousButtonState)
+  int currentButtonState = digitalRead(BUTTON_PIN);
+
+  // Update LCD only when the button state changes
+  if (currentButtonState != previousButtonState)
   {
-    delay(50);  // Debounce
+    delay(20);  // Simple switch debounce
 
-    buttonState = digitalRead(buttonPin);
+    currentButtonState = digitalRead(BUTTON_PIN);
 
-    lcd.setCursor(0, 1);
-    lcd.print("                ");
-
-    lcd.setCursor(0, 1);
-
-    if (buttonState == LOW)
+    if (currentButtonState != previousButtonState)
     {
-      lcd.print("Button: PRESSED");
-    }
-    else
-    {
-      lcd.print("Button: RELEASED");
-    }
+      lcd.setCursor(0, 1);
 
-    previousButtonState = buttonState;
+      if (currentButtonState == LOW)
+      {
+        lcd.print("Button: PRESSED ");
+      }
+      else
+      {
+        lcd.print("Button: RELEASED");
+      }
+
+      previousButtonState = currentButtonState;
+    }
   }
+
+  // Other controller tasks can also be performed here
 }
+
+// Timer1 Compare Match A Interrupt Service Routine
+ISR(TIMER1_COMPA_vect)
+{
+  ledState = !ledState;
+  digitalWrite(LED_PIN, ledState);
+}
+
+  
